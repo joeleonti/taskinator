fb38636cb55cfb13e9e0a2e2be060f56f9e5d356
Zorlock <joeleonti@gmail.com>
2020-11-08 15:05:39 -0500

    added ability to delete task

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var pageContentEl = document.querySelector("#page-content");
 var taskIdCounter = 0;
 
 var taskFormHandler = function (event) {
@@ -95,3 +96,23 @@ var createTaskActions = function (taskId) {
 };
 
 formEl.addEventListener("submit", taskFormHandler);
+
+var deleteTask = function (taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+};
+
+var taskButtonHandler = function (event) {
+    console.log(event.target);
+
+    if (event.target.matches(".delete-btn")) {
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
+
+
+
+
+
+pageContentEl.addEventListener("click", taskButtonHandler);
